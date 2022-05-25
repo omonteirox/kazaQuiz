befaccd54b650dd1933a1ec36fb5753d8bbe0736
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-05-25 10:34:49 -0300

    Tela de Cadastro e Login feitas, Tela Inicial está com as intents para a transição entre telas.

---
+++ app/src/main/java/com/kaza/kazaquiz/activityTelaInicial.java
@@ -0,0 +1,24 @@
+package com.kaza.kazaquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class activityTelaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+    public void telaLogin(View view){
+        Intent intent = new Intent(this, activityLogin.class);
+        startActivity(intent);
+    }
+    public void telaCadastro(View view){
+        Intent intent = new Intent(this, activityCadastro.class);
+        startActivity(intent);
+    }
+}
