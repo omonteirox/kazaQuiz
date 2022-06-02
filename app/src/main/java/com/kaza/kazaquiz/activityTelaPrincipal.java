ce207bba23abf281497b70dad12e00bb85c259af
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-06-02 07:56:26 -0300

    Adicionando botão de logOut

--- app/src/main/java/com/kaza/kazaquiz/activityTelaPrincipal.java
+++ app/src/main/java/com/kaza/kazaquiz/activityTelaPrincipal.java
@@ -2,13 +2,31 @@ package com.kaza.kazaquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class activityTelaPrincipal extends AppCompatActivity {
-
+    private Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+        iniciarComponentes();
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(activityTelaPrincipal.this, activityTelaInicial.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    private void iniciarComponentes(){
+        btnLogout = findViewById(R.id.btnLogout);
     }
 }
